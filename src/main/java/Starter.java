8165572e4b591ead9118c72db9f49cd42e7cea82
Flurin Brühwiler <47397416+FlurinBruehwiler@users.noreply.github.com>
2022-01-25 09:20:57 +0100

    update path

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -4,7 +4,7 @@ public class Starter {
     public static void main(String[] args) throws IOException {
         Cache objCache = new Cache();
         Reader reader = new Reader(objCache);
-        reader.readLernsetFiles("src/main/resources/test1.txt");
+        reader.readLernsetFiles("src/main/resources");
 
         MainMenu mainMenu = new MainMenu(objCache);
         mainMenu.showMainMenu();
